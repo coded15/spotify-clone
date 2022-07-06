ddf91c01099d8f35ec54bf8f5fc436ba82eb8178
coded15 <91874598+coded15@users.noreply.github.com>
2022-07-06 18:31:19 +0530

    created the main player component

---
+++ src/Player.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Body from './Body';
+import './Player.css';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+function Player() {
+    return (
+        <div className='player'>
+            <div className="player__body">
+                <Sidebar />
+                <Body />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Player
