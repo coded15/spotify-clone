5daf8e18ead7fc6ed0baa2582db466acd1ea0c2e
coded15 <91874598+coded15@users.noreply.github.com>
2022-07-06 18:28:04 +0530

    added token and api functionalities

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  //run code based on a given condition
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      })
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
+      })
+    }
+
+    console.log('I HAVE A TOKEN>>> ', token)
+  }, []);
+  // console.log('🧑', user);
+  // console.log('👽', token);
   return (
     //BEM
     <div className="App">
-      <Login />
+      {
+        token ? (
+          <Player spotify={spotify} />
+        ) : (
+          <Login />
+        )
+      }
 
     </div>
   );
