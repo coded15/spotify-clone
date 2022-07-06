1dc271469f34bb70e45005ce44e4aeba9fbf4385
coded15 <91874598+coded15@users.noreply.github.com>
2022-07-06 21:02:26 +0530

    added functionalities for icons and playlists

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
 import SidebarOption from './SidebarOption'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import { useDataLayerValue } from './DataLayer';
 
 function Sidebar() {
+    const [{ playlists }, dispatch] = useDataLayerValue();
+
     return (
         <div className='sidebar'>
             <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption Icon={HomeOutlinedIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+            <br />
+            <strong className='sidebar__title'>PLAYLISTS</strong>
+            <hr />
+
+            {playlists?.items?.map(playlist => (
+                <SidebarOption title={playlist.name} />
+            ))}
         </div>
     )
 }
