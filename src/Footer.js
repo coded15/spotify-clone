935dbad86bd03536fd99092c63c30e2ce4a9f2f6
coded15 <91874598+coded15@users.noreply.github.com>
2022-07-07 00:33:17 +0530

    added functionalities for the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import { Grid, Slider } from '@mui/material';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 
 function Footer() {
     return (
         <div className='footer'>
-            <h1>
-                I am the footer.
-            </h1>
+            <div className="footer__left">
+                <img className='footer__albumLogo' src="https://upload.wikimedia.org/wikipedia/commons/4/45/Usher.jpg" alt="" />
+                <div className="footer__songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className='footer__green' />
+                <SkipPreviousIcon className='footer__icon' />
+                <PlayCircleOutlineIcon fontSize='large' className='footer__icon' />
+                <SkipNextIcon className='footer__icon' />
+                <RepeatIcon className='footer__green' />
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                    <Grid item>
+                        <VolumeUpIcon />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
