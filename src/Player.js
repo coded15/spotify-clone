9531cf1f05319a383a1f48d4621b994289915bdc
coded15 <91874598+coded15@users.noreply.github.com>
2022-07-07 01:39:57 +0530

    Added functionalities for spotify header

--- src/Player.js
+++ src/Player.js
@@ -4,12 +4,12 @@ import './Player.css';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 
-function Player() {
+function Player({ spotify }) {
     return (
         <div className='player'>
             <div className="player__body">
                 <Sidebar />
-                <Body />
+                <Body spotify={spotify} />
             </div>
             <Footer />
         </div>
