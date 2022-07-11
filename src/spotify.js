63750872292f660ec2ea84089dbdd33bca2671e6
Vivekkr83 <75994974+Vivekkr83@users.noreply.github.com>
2022-07-11 20:10:01 +0530

    Fixed login issues

--- src/spotify.js
+++ src/spotify.js
@@ -1,4 +1,4 @@
-export const authEndpoint="https://accounts.spotify.com/authorize";
+export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const redirectUri = "http://localhost:3000/";
 
